b3efd10fb35c96f58f4e033f2248fc275394cec0
Shane-Geary <shane.geary@glowstik.com>
2023-03-20 10:12:21 -0600

    Maor - Fly to feature

--- src/GlowstikMap.js
+++ src/GlowstikMap.js
@@ -1,5 +1,5 @@
 // Copyright 2023 Glowstik Inc. All rights reserved!
-import {useState} from 'react'
+import {useState, useRef, useEffect} from 'react'
 
 import ReactMapGL from 'react-map-gl'
 
@@ -9,29 +9,51 @@ import 'mapbox-gl/dist/mapbox-gl.css'
 
 const GlowstikMap = () => {
 
+    const mapRef = useRef(null)
+
     const [viewport, setViewport] = useState({
         latitude: 0,
         longitude: 0,
         zoom: 1.5
     })
 
+    const [mapLoaded, setMapLoaded] = useState(false)
+    const [geoReceived, setGeoReceived] = useState(false)
+
     const [geoCoords, setGeoCoords] = useState({geoLat: null, geoLong: null})
 
-    const getCoordinates = () => {
-        getCurrentGeoPosition((position) => {
-          console.log(position)
-          setGeoCoords({geoLat: position.coords.latitude, geoLong: position.coords.longitude})
-        })
-    }
+    useEffect(() => {
+        console.log(mapRef.current)
+        const flyToAnimation = async () => {
+			if(mapLoaded && geoReceived) {
+				mapRef.current.flyTo({
+					center: [geoCoords.geoLong, geoCoords.geoLat], // The coordinates returned from geolocation api where we 'fly to'
+					zoom: 13, // The zoom level of the map that the flyTo stops on
+					preloadOnly: true,
+				})
+				await mapRef.current.once('idle')
+				mapRef.current.flyTo({
+					center: [geoCoords.geoLong, geoCoords.geoLat], // The coordinates returned from geolocation api where we 'fly to'
+					zoom: 13, // The zoom level of the map that the flyTo stops on
+					duration: 2500, // How long the animation takes from start to finish
+					easing: (t) => { // ease-out
+						return t
+					}
+				})
+			}
+		}
+		flyToAnimation()
+    }, [mapLoaded, geoReceived])
 
     return (
         <ReactMapGL
+            ref={mapRef}
             style={{
                 height: visualViewport.height,
                 width: visualViewport.width
             }}
             mapboxAccessToken={process.env.REACT_APP_MAPBOX_API_KEY}
-            mapStyle= {
+            mapStyle={
                 (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) ?
                     process.env.REACT_APP_MAPBOX_STYLE_URL_DARK :
                     process.env.REACT_APP_MAPBOX_STYLE_URL
@@ -41,7 +63,11 @@ const GlowstikMap = () => {
                 setViewport(e.viewState)
             }}
             onLoad={() => {
-                getCoordinates()
+                setMapLoaded(true)
+                getCurrentGeoPosition((position) => {
+                    setGeoCoords({geoLat: position.coords.latitude, geoLong: position.coords.longitude})
+                    setGeoReceived(true)
+                })
             }}
             {...viewport}
         >
