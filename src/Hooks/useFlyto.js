78ae6199c681791dbf6aaaf0e6823aacb9640661
Shane-Geary <shane.geary@glowstik.com>
2023-03-21 15:09:09 -0600

    Minor - Write comments for useFlyto hook

--- src/Hooks/useFlyto.js
+++ src/Hooks/useFlyto.js
@@ -1,6 +1,15 @@
 // Copyright 2023 Glowstik Inc. All rights reserved!
 import {useEffect, useState} from 'react'
 
+/**
+* Custom hook for animating the map flyTo functionality. Preloads a flyTo animation with coordinates obtained from the geolocation API. The hook also provides a click event handler to perform the flyTo animation on the map.
+* @param {ref} mapRef - The reference to the map object.
+* @param {Object} geoCoords - An object containing the geographic coordinates returned from the geolocation API.
+* @param {boolean} mapLoaded - A boolean indicating whether the map has been loaded.
+* @param {boolean} geoReceived - A boolean indicating whether the geographic coordinates have been received from the geolocation API.
+* @returns {Object} An object containing the preloaded flyTo state and a click event handler to perform the animation.
+*/
+
 const useFlyto = (mapRef, geoCoords, mapLoaded, geoReceived) => {
     const [flyToPreloaded, setFlyToPreloaded] = useState(false)
 
